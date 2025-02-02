7f500acb820a70475fb5352e9e35eda7d927be40
Oery <oery.dev@gmail.com>
2025-02-02 21:53:09 +0100

    Refactor

--- src/registry/client_command_actions.rs
+++ src/registry/client_command_actions.rs
@@ -1,28 +1,10 @@
-use std::io;
-
-use num_enum::{IntoPrimitive, TryFromPrimitive};
-
-use crate::serialization::{serialize_varint, VarIntReader};
 use crate::serialization::{Deserialize, Serialize};
+use macros::{Deserialize, Serialize};
 
-#[derive(Debug, Eq, PartialEq, TryFromPrimitive, IntoPrimitive, Copy, Clone)]
-#[repr(i32)]
+#[derive(Debug, Eq, PartialEq, Copy, Clone, Serialize, Deserialize)]
+#[encoding("u8")]
 pub enum ClientCommandActions {
     PerformRespawn = 0,
     RequestStats = 1,
     TakingInventoryAchievement = 2,
-}
-
-impl Deserialize for ClientCommandActions {
-    fn deserialize<R: io::Read>(reader: &mut R) -> io::Result<Self> {
-        ClientCommandActions::try_from(reader.read_varint()?)
-            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
-    }
-}
-
-impl Serialize for ClientCommandActions {
-    fn serialize(&self, buf: &mut dyn io::Write) -> io::Result<()> {
-        let value = i32::from(*self);
-        serialize_varint(&value, buf)
-    }
 }
--- src/registry/client_settings_chat_modes.rs
+++ src/registry/client_settings_chat_modes.rs
@@ -1,28 +1,10 @@
-use std::io;
-
-use byteorder::{ReadBytesExt, WriteBytesExt};
-use num_enum::{IntoPrimitive, TryFromPrimitive};
-
 use crate::serialization::{Deserialize, Serialize};
+use macros::{Deserialize, Serialize};
 
-#[derive(Debug, Eq, PartialEq, TryFromPrimitive, IntoPrimitive, Copy, Clone)]
-#[repr(u8)]
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize, Copy, Clone)]
+#[encoding("u8")]
 pub enum ChatModes {
     Full = 0,
     CommandsOnly = 1,
     Hidden = 2,
-}
-
-impl Deserialize for ChatModes {
-    fn deserialize<R: io::Read>(reader: &mut R) -> io::Result<Self> {
-        let byte = reader.read_u8()?;
-        ChatModes::try_from(byte).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
-    }
-}
-
-impl Serialize for ChatModes {
-    fn serialize(&self, buf: &mut dyn std::io::Write) -> std::io::Result<()> {
-        let byte = u8::from(*self);
-        buf.write_u8(byte)
-    }
 }
