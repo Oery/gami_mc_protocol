591db6106476e004bd4c6003a6bf66c165edc900
Oery <oery.dev@gmail.com>
2025-02-07 01:11:11 +0100

    Simplify Animation Packet

--- src/packets/play/server/11_animation.rs
+++ src/packets/play/server/11_animation.rs
@@ -1,8 +1,7 @@
-use crate::packets::Packet;
-use crate::serialization::{deserialize_varint, serialize_varint};
 use gami_macros::{packet, Deserialize, Serialize};
 
-use byteorder::{ReadBytesExt, WriteBytesExt};
+use crate::packets::Packet;
+use crate::serialization::{deserialize_varint, serialize_varint};
 
 #[packet(0x0B, server)]
 pub struct Animation {
@@ -11,47 +10,13 @@ pub struct Animation {
     pub animation: AnimationKind,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone, Copy)]
+#[encoding("u8")]
 pub enum AnimationKind {
-    SwingArm,
-    TakeDamage,
-    LeaveBed,
-    Eat,
-    CriticalEffect,
-    MagicalCriticalEffect,
-}
-
-impl Deserialize for AnimationKind {
-    fn deserialize<R: std::io::Read>(reader: &mut R) -> std::io::Result<Self> {
-        use AnimationKind::*;
-
-        let action = reader.read_u8()?;
-        match action {
-            0 => Ok(SwingArm),
-            1 => Ok(TakeDamage),
-            2 => Ok(LeaveBed),
-            3 => Ok(Eat),
-            4 => Ok(CriticalEffect),
-            5 => Ok(MagicalCriticalEffect),
-            _ => Err(std::io::Error::new(
-                std::io::ErrorKind::InvalidData,
-                format!("Invalid animation kind : {action}"),
-            )),
-        }
-    }
-}
-
-impl Serialize for AnimationKind {
-    fn serialize(&self, buf: &mut dyn std::io::Write) -> std::io::Result<()> {
-        use AnimationKind::*;
-
-        match self {
-            SwingArm => buf.write_u8(0),
-            TakeDamage => buf.write_u8(1),
-            LeaveBed => buf.write_u8(2),
-            Eat => buf.write_u8(3),
-            CriticalEffect => buf.write_u8(4),
-            MagicalCriticalEffect => buf.write_u8(5),
-        }
-    }
+    SwingArm = 0,
+    TakeDamage = 1,
+    LeaveBed = 2,
+    Eat = 3,
+    CriticalEffect = 4,
+    MagicalCriticalEffect = 5,
 }
