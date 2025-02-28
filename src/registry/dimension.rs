38ba4c463432686300ff4ff132ad7cae3a73e380
Oery <oery.dev@gmail.com>
2025-02-28 17:47:24 +0100

    Add default dimension

--- src/registry/dimension.rs
+++ src/registry/dimension.rs
@@ -2,10 +2,11 @@ use gami_macros::{Deserialize, Serialize};
 
 use crate::serialization::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone, Copy)]
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone, Copy, Default)]
 #[encoding("i8")]
 pub enum Dimension {
     Nether = -1,
+    #[default]
     Overworld = 0,
     End = 1,
 }
