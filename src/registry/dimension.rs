5d354c11bd276983f7827385d4c5e9b33eb3e05c
Oery <oery.dev@gmail.com>
2025-05-24 14:38:46 +0200

    add traits to dimension enum

--- src/registry/dimension.rs
+++ src/registry/dimension.rs
@@ -1,8 +1,9 @@
+use derive_more::Display;
 use gami_macros::{Deserialize, Serialize};
 
 use crate::serialization::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone, Copy, Default)]
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone, Copy, Default, Display)]
 #[encoding("i8")]
 pub enum Dimension {
     Nether = -1,
